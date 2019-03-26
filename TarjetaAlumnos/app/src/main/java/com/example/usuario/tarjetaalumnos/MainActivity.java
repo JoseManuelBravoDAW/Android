fe1b0576146de61e52c9821e13aec30f3c6c94e3
JoseManuelBravoDAW <josemanuelbravomartinezdaw@gmail.com>
2019-03-26 12:41:57 +0100

    Comenzado proyecto tarjeta alumnos

---
+++ TarjetaAlumnos/app/src/main/java/com/example/usuario/tarjetaalumnos/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.usuario.tarjetaalumnos;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment fragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_puntuacion:
+                    fragment = new PuntuacionFragment();
+                    break;
+                case R.id.navigation_noticias:
+                    fragment = new NoticiasFragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        }
+    };
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
